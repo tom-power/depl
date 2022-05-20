e5eef5074b28fc816cdb63b99768d1d993604829
Tom Power <tp21@tuta.io>
2022-05-20 23:00:06 +0100

    fix

--- go/flanalystsh_test/command_test.go
+++ go/flanalystsh_test/command_test.go
@@ -9,7 +9,7 @@ import (
 func Test_command(t *testing.T) {
 	t.Run("can run command", func(t *testing.T) {
 		for command, expected := range flanalystsh.Commands {
-			sh := flanalystsh.GetCommand(command)
+			sh, _ := flanalystsh.GetCommand(command)
 			if !strings.Contains(sh, expected) {
 				t.Errorf("'%v' should contain '%v'", sh, expected)
 			}
