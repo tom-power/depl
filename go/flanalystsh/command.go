57ea672c322fea8fb9fc42b9513360a725ac998e
Tom Power <tp21@tuta.io>
2022-05-20 22:34:56 +0100

    add catLog/Event

--- go/flanalystsh/command.go
+++ go/flanalystsh/command.go
@@ -16,6 +16,20 @@ sh/flanalyst.sh pull \
 && sh/flanalyst.sh build \
 && sh/flanalyst.sh dockerUp
 `
+const catLog = `
+#!/bin/bash
+while read line; do
+  if jq -e . >/dev/null 2>&1 <<<"$line"; then
+    echo $line | jq
+  else
+    echo $line
+  fi
+done < <(tail docker/data/log)
+`
+const catEvent = `
+#!/bin/bash
+jq -R 'try fromjson catch .' < docker/data/event
+`
 
 var Commands = map[string]string{
 	"test":           "sh/copyLibs.sh && ./gradlew test -Denv=local --parallel",
@@ -26,7 +40,9 @@ var Commands = map[string]string{
 	"dockerUp":       "docker-compose -f docker/docker-compose-app.yml up --build --detach",
 	"dockerDown":     "docker-compose -f docker/docker-compose-app.yml down",
 	"deploy":         deploy,
-	"remote":         remote}
+	"remote":         remote,
+	"catLog":         catLog,
+	"catEvent":       catLog}
 
 var GetCommand = func(command string) string {
 	return Commands[command]
