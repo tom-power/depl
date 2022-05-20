690a20e883e88acafddb5de73c99c490ce7c0667
Tom Power <tpower21@gmail.com>
2022-05-20 21:54:20 +0100

    messing with remote

--- go/flanalystsh/command.go
+++ go/flanalystsh/command.go
@@ -6,18 +6,18 @@ dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $dir/.env
 ssh -t $remoteUser@$remoteHost "\
 cd $deployDir && \
-sh/$1.sh $2"
+sh/flanalyst.sh $2"
 `
 
 var Commands = map[string]string{
 	"test":           "sh/copyLibs.sh && ./gradlew test -Denv=local --parallel",
 	"push":           "git push origin master -f",
-	"testPushDeploy": "sh/test.sh && sh/push.sh && sh/remote/run.sh deploy",
+	"testPushDeploy": "sh/flanalyst.sh test && sh/flanalyst.sh push && sh/flanalyst.sh remote deploy",
 	"pull":           "git fetch --all && git reset --hard origin/master",
+	"build":          "./gradlew :app:shadowJar --no-daemon",
 	"dockerUp":       "docker-compose -f docker/docker-compose-app.yml up --build --detach",
 	"dockerDown":     "docker-compose -f docker/docker-compose-app.yml down",
-	"deploy":         "sh/pull.sh && sh/deployCopyLibs.sh && sh/build.sh && sh/dockerUp.sh",
-	"build":          "./gradlew :app:shadowJar --no-daemon",
+	"deploy":         "sh/flanalyst.sh pull && sh/deployCopyLibs.sh && sh/flanalyst.sh build && sh/flanalyst.sh dockerUp",
 	"remote":         remote}
 
 var GetCommand = func(command string) string {
