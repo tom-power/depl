f1d6624bc56bb483242b44c43edb9c39047ed81c
Tom Power <tp21@tuta.io>
2022-05-20 23:44:35 +0100

    keep perimssions

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 $root/build.sh
 
-cp $root/go/build/flanalystsh ~/proj/os/flanalyst.ovh/
-cp $root/sh/flanalyst.sh ~/proj/os/flanalyst.ovh/
+cp -p $root/go/build/flanalystsh ~/proj/os/flanalyst.ovh/
+cp -p $root/sh/flanalyst.sh ~/proj/os/flanalyst.ovh/
 
 echo "installed"
