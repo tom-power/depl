fdbaad0a4651dd53c31cff91f4b13c01ae83188d
Tom Power <tp21@tuta.io>
2022-05-21 07:07:28 +0100

    remote install

---
+++ installRemote.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+. $root/.env &&
+ssh -t $remoteUser@$remoteHost "\
+cd $deployDir && \
+git fetch --all && git reset --hard origin/master && \
+cd ./go &&
+./build.sh &&
+cd ../
+cp -p ./go/build/flanalystsh ../
+cp -p ./sh/flanalyst.sh ../../"
+
+
+echo "installed remote"
