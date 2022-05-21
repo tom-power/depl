e36f8719009158610d3587569e3a53671863662d
Tom Power <tpower21@gmail.com>
2022-05-21 08:27:55 +0100

    add list command

--- go/flanalystsh_test/command_test.go
+++ go/flanalystsh_test/command_test.go
@@ -8,11 +8,22 @@ import (
 
 func Test_command(t *testing.T) {
 	t.Run("can run command", func(t *testing.T) {
-		for command, expected := range flanalystsh.Commands {
+		for command, value := range flanalystsh.Commands {
 			sh, _ := flanalystsh.GetCommand(command)
-			if !strings.Contains(sh, expected) {
-				t.Errorf("'%v' should contain '%v'", sh, expected)
+			if !strings.Contains(sh, value) {
+				t.Errorf("'%v' should contain '%v'", sh, value)
 			}
+		}
+	})
+	t.Run("can list commands", func(t *testing.T) {
+		sh, _ := flanalystsh.GetCommand("list")
+		for command, _ := range flanalystsh.Commands {
+			if !strings.Contains(sh, command) {
+				t.Errorf("'%v' should contain '%v'", sh, command)
+			}
+		}
+		if !strings.Contains(sh, "list") {
+			t.Errorf("'%v' should contain '%v'", sh, "list")
 		}
 	})
 }
