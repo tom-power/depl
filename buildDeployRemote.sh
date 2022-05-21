cc8267985d4f99af787cdce7687f833384c89555
Tom Power <tpower21@gmail.com>
2022-05-21 09:00:09 +0100

    split remote/local deploy

---
+++ buildDeployRemote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+$root/buildDeploy.sh
+
+# remote
+git push origin master -f
+. $root/.env &&
+ssh -t $remoteUser@$remoteHost "\
+cd $deployDir && \
+git fetch --all && git reset --hard origin/master && \
+cd ./go &&
+./build.sh &&
+cd ../
+cp -p ./go/build/flanalystsh ../
+cp -p ./sh/flanalyst.sh ../"
