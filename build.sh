2f2301950a4bb6b054a0e6a769b32ee961932f92
Tom Power <tp21@tuta.io>
2022-05-21 07:33:14 +0100

    add buildDeploy

--- build.sh
+++ build.sh
@@ -2,5 +2,3 @@
 root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $root/go && ./build.sh
-
-echo "built"
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-$root/build.sh
-
-cp -p $root/go/build/flanalystsh $root/../
-cp -p $root/sh/flanalyst.sh $root/../
