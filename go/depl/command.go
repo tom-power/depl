bf252ed5a034e2c6f825350228a312e856aa733b
Tom Power <tpower21@gmail.com>
2022-05-22 09:11:51 +0100

    add buildLib

--- go/depl/command.go
+++ go/depl/command.go
@@ -6,14 +6,15 @@ import (
 )
 
 const buildLib = `
+. sh/.env &&
 ./gradlew build &&
 
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT.jar ../$lib-lib.jar
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT-sources.jar ../$lib-lib-sources.jar
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT-javadoc.jar ../$lib-lib-javadoc.jar
+cp lib/lib/build/libs/lib-1.0-SNAPSHOT.jar ../$lib-lib.jar &&
+cp lib/lib/build/libs/lib-1.0-SNAPSHOT-sources.jar ../$lib-lib-sources.jar &&
+cp lib/lib/build/libs/lib-1.0-SNAPSHOT-javadoc.jar ../$lib-lib-javadoc.jar &&
 
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT.jar ../$lib-testing.jar
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT-sources.jar ../$lib-testing-sources.jar
+cp lib/testing/build/libs/testing-1.0-SNAPSHOT.jar ../$lib-testing.jar &&
+cp lib/testing/build/libs/testing-1.0-SNAPSHOT-sources.jar ../$lib-testing-sources.jar &&
 cp lib/testing/build/libs/testing-1.0-SNAPSHOT-javadoc.jar ../$lib-testing-javadoc.jar
 `
 
