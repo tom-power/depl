2beff6f97bc5ed99656034f5ebfa6b034a4df972
Tom Power <tpower21@gmail.com>
2022-05-23 21:56:42 +0100

    cp local in build

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $root/go && ./build.sh
+
+# local
+cp -p $root/go/build/depl ~/bin/
--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@ root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 $root/build.sh
 
-# local
-cp -p $root/go/build/depl ~/bin/
-
 # remote
 . $root/.env &&
 scp ./go/build/depl $remoteUser@$remoteHost:$deployDir
