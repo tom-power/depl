71d79ce4f0ef7710f535f74762757b7f874323d0
Tom Power <tpower21@gmail.com>
2022-05-26 08:23:36 +0100

    add libPath

--- go/depl/sh/buildLib.sh
+++ go/depl/sh/buildLib.sh
@@ -1,10 +1,10 @@
 . sh/.env &&
 ./gradlew build &&
 
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT.jar ../$lib-lib.jar &&
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT-sources.jar ../$lib-lib-sources.jar &&
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT-javadoc.jar ../$lib-lib-javadoc.jar &&
+cp lib/lib/build/libs/lib-1.0-SNAPSHOT.jar $libPath$lib-lib.jar &&
+cp lib/lib/build/libs/lib-1.0-SNAPSHOT-sources.jar $libPath$lib-lib-sources.jar &&
+cp lib/lib/build/libs/lib-1.0-SNAPSHOT-javadoc.jar $libPath$lib-lib-javadoc.jar &&
 
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT.jar ../$lib-testing.jar &&
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT-sources.jar ../$lib-testing-sources.jar &&
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT-javadoc.jar ../$lib-testing-javadoc.jar
+cp lib/testing/build/libs/testing-1.0-SNAPSHOT.jar $libPath$lib-testing.jar &&
+cp lib/testing/build/libs/testing-1.0-SNAPSHOT-sources.jar $libPath$lib-testing-sources.jar &&
+cp lib/testing/build/libs/testing-1.0-SNAPSHOT-javadoc.jar $libPath$lib-testing-javadoc.jar
