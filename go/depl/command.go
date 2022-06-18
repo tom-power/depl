9288235ca74f451e2972352562964b8e1dcc0008
Tom Power <tp21@tuta.io>
2022-06-18 15:54:24 +0100

    tidy

--- go/depl/command.go
+++ go/depl/command.go
@@ -9,25 +9,25 @@ var GetCommand = func(cmd string) (string, error) {
 	if cmd == "list" {
 		return commandList()
 	}
-  return command(cmd)
+	return command(cmd)
 }
 
 func commandList() (string, error) {
-  return strings.Join(keys(Commands), " ") + " list", nil
+	return strings.Join(keys(Commands), " ") + " list", nil
 }
 
 func keys[K comparable, V any](m map[K]V) []K {
-    keys := make([]K, 0, len(m))
-    for k := range m {
-        keys = append(keys, k)
-    }
-    return keys
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
 func command(cmd string) (string, error) {
-  command := Commands[cmd]
-  if command == "" {
-    return "", errors.New("unknown command " + command)
-  }
-  return command, nil
+	command := Commands[cmd]
+	if command == "" {
+		return "", errors.New("unknown command " + command)
+	}
+	return command, nil
 }
