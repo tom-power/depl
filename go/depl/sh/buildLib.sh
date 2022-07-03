05cb323c11d34681a2ef83f807bd21e2886d2e5e
Tom Power <tp21@tuta.io>
2022-07-03 11:51:52 +0100

    flanalyst-lib -> lib

--- go/depl/sh/buildLib.sh
+++ go/depl/sh/buildLib.sh
@@ -1,10 +1,1 @@
-. sh/.env &&
-./gradlew build &&
-
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT.jar $libPath$lib-lib.jar &&
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT-sources.jar $libPath$lib-lib-sources.jar &&
-cp lib/lib/build/libs/lib-1.0-SNAPSHOT-javadoc.jar $libPath$lib-lib-javadoc.jar &&
-
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT.jar $libPath$lib-testing.jar &&
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT-sources.jar $libPath$lib-testing-sources.jar &&
-cp lib/testing/build/libs/testing-1.0-SNAPSHOT-javadoc.jar $libPath$lib-testing-javadoc.jar
+./gradlew build
