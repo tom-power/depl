06a9a4daea0415d588a5c82be913cf24ab0c63ca
Tom Power <tp21@tuta.io>
2023-02-24 08:38:59 +0000

    also cp completions

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# build
+cd ./go && ./build.sh
+
+# copy to remote
+cd ../
 source sh/.env &&
-scp -p ./go/build/_depl ./config/depl.sh $remoteUser@$remoteHost:$deployDir
+scp -p ./go/build/_depl ./config/depl.sh $remoteUser@$remoteHost:$deployDir &&
+scp -p ./config/.oh-my-zsh/custom/completions/_depl $remoteUser@$remoteHost:~/config/.oh-my-zsh/custom/completions/
