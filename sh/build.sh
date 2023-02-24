59766dd16a055836bfb7e1a3f2c1f62002524458
Tom Power <tpower21@gmail.com>
2023-02-24 08:38:59 +0000

    also cp completions

--- sh/build.sh
+++ sh/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# build
 cd ./go && ./build.sh
 
 # copy to local
-cd ../ && cp -p ./go/build/_depl ./config/depl.sh ~/bin/
+cd ../ &&
+cp -p ./go/build/_depl ./config/depl.sh ~/bin/ &&
+cp -p ./config/.oh-my-zsh/custom/completions/_depl ~/config/.oh-my-zsh/custom/completions/
