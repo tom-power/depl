ef84042fefd7246ed5bc3e3035d8d10913b3cbb4
Tom Power <tpower21@gmail.com>
2023-03-03 21:11:20 +0000

    don't copy completions

--- sh/build.sh
+++ sh/build.sh
@@ -4,5 +4,5 @@ cd ./go && ./build.sh
 
 # copy to local
 cd ../ &&
-cp -p ./go/build/_depl ./config/depl.sh ~/bin/ &&
-cp -p ./config/.oh-my-zsh/custom/completions/_depl ~/config/.oh-my-zsh/custom/completions/
+cp -p ./go/build/_depl ./config/depl.sh ~/bin/
+# cp -p ./config/.oh-my-zsh/custom/completions/_depl ~/config/.oh-my-zsh/custom/completions/
