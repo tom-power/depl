30eadbe9c25e3b2761abf8036a12a1eb2b98cd59
Tom Power <tp21@tuta.io>
2023-04-07 07:14:55 +0100

    mkdir on copyLibs

--- go/depl/sh/copyLibs.sh
+++ go/depl/sh/copyLibs.sh
@@ -1,5 +1,9 @@
 . sh/.env &&
 
+if [[ ! -d ./libs/ ]]; then
+  mkdir ./libs/
+fi
+
 cp -p $libPath/lib/lib/build/libs/lib-1.0-SNAPSHOT.jar ./libs/lib.jar &&
 cp -p $libPath/lib/lib/build/libs/lib-1.0-SNAPSHOT-sources.jar ./libs/lib-sources.jar &&
 cp -p $libPath/lib/lib/build/libs/lib-1.0-SNAPSHOT-javadoc.jar ./libs/lib-javadoc.jar &&
--- go/depl/sh/deployCopyLibs.sh
+++ go/depl/sh/deployCopyLibs.sh
@@ -1,3 +1,7 @@
 . sh/.env &&
 
+if [[ ! -d ./libs/ ]]; then
+  mkdir ./libs/
+fi
+
 cp -p $libPath/lib/lib/build/libs/lib-1.0-SNAPSHOT.jar ./libs/lib.jar
