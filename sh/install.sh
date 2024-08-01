4f71c217d5cde90a58db492860911c0f09a1bec6
Tom Power <tp21@tuta.io>
2024-08-01 09:01:13 +0100

    add submodule update

--- sh/install.sh
+++ sh/install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-# build
-sh/build.sh
-
 deplBinPath=./go/build/_depl
 deplShPath=./config/depl.sh
 deplCompletionPath=./config/_depl
 
+function echoWithHyphens() {
+    echo "--------------$1--------------"
+}
+
+# build
+echoWithHyphens "start"
+sh/build.sh
+echoWithHyphens "built"
+
 # copy to local
 cp -p $deplBinPath $deplShPath ~/bin/
 #cp -p $deplCompletionPath ~/config/.oh-my-zsh/custom/completions/
+echoWithHyphens "copied to local"
 
 # copy to remote
 source sh/.env &&
 scp -p $deplBinPath $deplShPath $remoteUser@$remoteHost:$deployDir
+echoWithHyphens "copied to remote"
 #scp -p $deplCompletionPath $remoteUser@$remoteHost:~/config/.oh-my-zsh/custom/completions/
+echoWithHyphens "end"
