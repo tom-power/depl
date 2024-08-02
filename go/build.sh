ccf0138c168295de29c8681a18f05acaedaa2b50
Tom Power <tp21@tuta.io>
2024-08-02 20:52:44 +0100

    build.sh out of go

--- go/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-go test -count=1 ./depl/ &&
-go test -count=1 ./depl_test/ &&
-if [[ ! -d ./build ]]; then
-  mkdir -p ./build
-fi
-go build -o ./build/_depl main.go
--- sh/build.sh
+++ sh/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # build
-cd ./go && ./build.sh
+cd ./go && 
+go test -count=1 ./depl/ &&
+go test -count=1 ./depl_test/ &&
+if [[ ! -d ./build ]]; then
+  mkdir -p ./build
+fi
+go build -o ./build/_depl main.go
