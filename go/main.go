3033ca18c12e8321ec1bc9cfb7e4d4b8a20031c1
Tom Power <tpower21@gmail.com>
2024-08-02 19:51:30 +0100

    move explain echo to go

--- go/main.go
+++ go/main.go
@@ -9,9 +9,18 @@ import (
 )
 
 func main() {
-	script, err := depl.ScriptFor(os.Args[1])
+	script, err := depl.Run(os.Args[1], contains(os.Args, "--explain"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf(script)
+}
+
+func contains(args []string, target string) bool {
+	for _, arg := range args {
+		if arg == target {
+			return true
+		}
+	}
+	return false
 }
