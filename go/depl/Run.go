bad31d58ef43b0e82b76a3f2132c96f8449959bf
Tom Power <tp21@tuta.io>
2024-08-02 20:12:04 +0100

    tidy

--- go/depl/Run.go
+++ go/depl/Run.go
@@ -3,7 +3,6 @@ package depl
 import (
 	"errors"
 	"fmt"
-	"sort"
 	"strings"
 )
 
@@ -25,21 +24,6 @@ var Run = func(command string, explain bool) (string, error) {
 
 func commands() string {
 	return strings.Join(sorted(keys(CommandsToScripts)), " ") + " list"
-}
-
-func sorted(input []string) []string {
-	sorted := make([]string, len(input))
-	copy(sorted, input)
-	sort.Strings(sorted)
-	return sorted
-}
-
-func keys[K comparable, V any](m map[K]V) []K {
-	keys := make([]K, 0, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	return keys
 }
 
 func scriptFor(command string) (string, error) {
--- go/main.go
+++ go/main.go
@@ -9,18 +9,9 @@ import (
 )
 
 func main() {
-	script, err := depl.Run(os.Args[1], contains(os.Args, "--explain"))
+	script, err := depl.Run(os.Args[1], depl.Contains(os.Args, "--explain"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf(script)
-}
-
-func contains(args []string, target string) bool {
-	for _, arg := range args {
-		if arg == target {
-			return true
-		}
-	}
-	return false
+	fmt.Print(script)
 }
